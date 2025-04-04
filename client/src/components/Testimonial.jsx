a08f45635da00ff12d265738e31be99d213e64da
ritik singh <ritikrajsingh60224@gmail.com>
2025-04-05 00:29:31 +0530

    alingnment setup of testimonial

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets, testimonialsData } from '../assets/assets'
-import { delay,motion } from 'motion/react'
+import {motion } from 'motion/react'
 
 const Testimonial = () => {
   return (
@@ -16,8 +16,8 @@ const Testimonial = () => {
         <p className='text-gray-500 mb-12'>What our customers say</p>
         <div className='flex flex-wrap gap-6'>
             {testimonialsData.map((testimonial,index)=>(
-                <div key={index}>
-                    <div>
+                <div key={index} className='bg-white/20 p-20 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all'>
+                    <div className='flex flex-col items-center'>
                         <img src={testimonial.image} alt="" className='rounded-full w-14' />
                         <h2 className='text-xl font-semibold mt-3'>{testimonial.name}</h2>
                         <p  className='text-gray-500 mb-4'>{testimonial.role}</p>

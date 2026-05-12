d664c5a5ccb0333d6ccc08266ea983de7884e4df
ritik singh <ritikrajsingh60224@gmail.com>
2026-05-12 16:19:27 +0530

    minor bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import {BrowserRouter,Routes , Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Result from "./pages/Result"
-import Buycredit from "./pages/BuyCredit"
+import BuyCredit from "./pages/BuyCredit"
 import Footer from "./components/Footer"
 import Login from "./components/Login"
 import { useContext } from "react"
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/Result" element={<Result/>} />
-        <Route path="/Buy" element={<Buycredit/>} />
+        <Route path="/Buy" element={<BuyCredit/>} />
 
       </Routes>
        <Footer />
